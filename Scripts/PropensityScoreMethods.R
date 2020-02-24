7ef6ea8332788ab3a3ea8d9c5ae58fd5ea3fe6b0
ngyope <ngyope@yahoo.co.jp>
2020-02-25 00:18:04 +0900

    Add

--- Scripts/PropensityScoreMethods.R
+++ Scripts/PropensityScoreMethods.R
@@ -1,53 +1,98 @@
 library(tidyverse)
+library(haven)
 library(MatchIt)
 library(WeightIt)
 library(cobalt)
 
-# get the lalonde data
-data(lalonde)
-summary(lalonde)
+# This scripts uses the famous Lalonde data. So, let's get it.
+cps1_data <- read_dta("https://users.nber.org/~rdehejia/data/cps_controls.dta")
+cps3_data <- read_dta("https://users.nber.org/~rdehejia/data/cps_controls3.dta")
+nswdw_data <- read_dta("https://users.nber.org/~rdehejia/data/nsw_dw.dta")
 
-# define formulas
-# formula of outcome model
-frml_out <- as.formula(re78 ~ treat + age + educ + race + married + nodegree + re74 + I(re74^2))
-# formula of treatment model
-frml_treat <- as.formula(treat ~ age + educ + race + married + nodegree + re74 + I(re74^2))
+# Extract the treatment group from NSW Data and treat it as a treatment group in CPS1.
+cps1_nsw_data <- nswdw_data %>% filter(treat==1) %>% rbind(cps1_data)
+# Extract the treatment group from NSW Data and treat it as a treatment group in CPS3.
+cps3_nsw_data <- nswdw_data %>% filter(treat==1) %>% rbind(cps3_data)
 
-# matching methods
+
+
+# Define the formulas previously.
+# Formula of outcome model
+frml_out <- as.formula(re78 ~ treat + age + education + black + hispanic + married + nodegree + re74 + re75 + I(re74^2) + I(re75^2))
+
+# Formula of treatment model
+frml_treat <- as.formula(treat ~ age + education + black + hispanic + married + nodegree + re74 + re75 + I(re74^2) + I(re75^2))
+
+
+
+# Create matching models. We're going to estimate the ATT.
+# Therefore, discard the control units whose propensity score are smaller than the min of treatment units' ps.
+
 # nearest neighbors
-m_out_nn_1 <- matchit(frml_treat, lalonde, method="nearest", replace=TRUE, ratio=1, discard="control")
-m_out_nn_4 <- matchit(frml_treat, lalonde, method="nearest", replace=TRUE, ratio=4, discard="control")
+m_out_nn_1 <- matchit(frml_treat, cps1_nsw_data, method="nearest", ratio=1, replace=TRUE)
+m_out_nn_4 <- matchit(frml_treat, cps1_nsw_data, method="nearest", ratio=4, replace=TRUE)
+
 # subclassification
-m_out_subclass <- matchit(frml_treat, lalonde, method="subclass",subclass=5, discard="control")
-# caliper
-m_out_caliper <- matchit(frml_treat, lalonde, method="nearest", replace=TRUE, caliper=.025, ratio=1, discard="control")
+m_out_subclass <- matchit(frml_treat, cps1_nsw_data, method="subclass", subclass=10)
+
+# caliper matching
+m_out_caliper <- matchit(frml_treat, cps1_nsw_data, method="nearest", replace=TRUE, caliper=.01, ratio=1)
 
 # weighting methods
-w_out_ps <- weightit(frml_treat, lalonde, method="ps", estimand="ATT")
-w_out_gbm <- weightit(frml_treat, lalonde, method="gbm", stop.method="es.max", estimand="ATT")
-w_out_cbps <- weightit(frml_treat, lalonde, method="ps", estimand="ATT")
+w_out_ps <- weightit(frml_treat, cps1_nsw_data, method="ps", estimand="ATT")
+w_out_gbm <- weightit(frml_treat, cps1_nsw_data, method="gbm", stop.method="es.mean", estimand="ATT", bag.fraction=0.7, cv.folds=5)
+w_out_cbps <- weightit(frml_treat, cps1_nsw_data, method="cbps", estimand="ATT")
+summary(w_out_ps)
+summary(w_out_gbm)
+summary(w_out_cbps)
+w_out_ps_trm <- w_out_ps %>% trim(at=5, lower=TRUE)
+w_out_gbm_trm <- w_out_gbm %>% trim(at=2, lower=TRUE)
+w_out_cbps_trm <- w_out_cbps %>% trim(at=5, lower=TRUE)
+summary(w_out_ps_trm)
+summary(w_out_gbm_trm)
+summary(w_out_cbps_trm)
 
-# check the covariate balance
-love.plot(frml_treat, data=lalonde,
+# Check the covariate balance by love.plot() in "cobalt" package.
+# matching models
+love.plot(frml_treat, data=cps1_nsw_data,
           weights=data.frame(Nearest_Neighbor_1=get.w(m_out_nn_1),
                              Nearest_Neighbor_4=get.w(m_out_nn_4),
                              Subclassification=get.w(m_out_subclass),
-                             Caliper=get.w(m_out_caliper),
-                             PS_Weighting=get.w(w_out_ps)),
-          method=c("matching", "matching", "matching", "matching", "weighting"),
+                             Caliper=get.w(m_out_caliper)),
+          method=c("matching", "matching", "matching", "matching"),
+          binary="std", s.d.denom="treated", grid=FALSE, threshold=.1, limits=c(0,1))
+
+# Weighting models
+love.plot(frml_treat, data=cps1_nsw_data,
+          weights=data.frame(PS=get.w(w_out_ps_trm),
+                             GBM=get.w(w_out_gbm_trm),
+                             CBPS=get.w(w_out_cbps_trm)),
+          method=c("weighting", "weighting", "weighting"),
           binary="std", s.d.denom="treated", grid=FALSE, threshold=.1, limits=c(0,1))
 
 # check the covariate balance per covariate
 bal.plot(m_out_nn_1, var.name="distance", which="both", type="histogram", mirror=TRUE)
 
-# get the ATT
+
+
+# Get the estimated ATT via the linear model.
+# matching methods
 matched_data <- list(match.data(m_out_nn_1),
                      match.data(m_out_nn_4),
                      match.data(m_out_subclass),
                      match.data(m_out_caliper))
-model_label <- c("nearest neighbor ratio 1", "nearest neighbor ratio 4", "subclassification", "caliper")
+model_label1 <- c("nearest neighbor ratio 1", "nearest neighbor ratio 4", "subclassification", "caliper")
 
 for(i in 1:length(matched_data)){
-  print(model_label[[i]])
+  print(model_label1[[i]])
   print(lm(re78 ~ treat, data=matched_data[[i]]) %>% broom::tidy())
+}
+
+# weighting methods
+weighted_data <- list(w_out_ps_trm, w_out_gbm_trm, w_out_cbps_trm)
+model_label2 <- c("PS", "GBM", "CBPS")
+
+for(i in 1:length(weighted_data)){
+  print(model_label2[[i]])
+  print(lm(re78 ~ treat, data=cps1_nsw_data, weights=weighted_data[[i]]$weights) %>% broom::tidy())
 }
